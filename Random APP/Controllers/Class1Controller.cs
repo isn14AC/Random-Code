42da9cb55be824cb93070cf7c2be29b2e9cae165
Artūras Černiauskas <s023368@ad.viko.lt>
2016-07-19 12:50:45 +0300

    Fresh Start

---
+++ Random APP/Controllers/Class1Controller.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Random_APP.Models;
+
+namespace Random_APP.Controllers
+{
+    public class Class1Controller : Controller
+    {
+        private ClassDBContext db = new ClassDBContext();
+
+        // GET: Class1
+        public ActionResult Index()
+        {
+            return View(db.Classes.ToList());
+        }
+
+        // GET: Class1/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Class1 class1 = db.Classes.Find(id);
+            if (class1 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(class1);
+        }
+
+        // GET: Class1/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Class1/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "NumberID,Name,LastName,age")] Class1 class1)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Classes.Add(class1);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(class1);
+        }
+
+        // GET: Class1/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Class1 class1 = db.Classes.Find(id);
+            if (class1 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(class1);
+        }
+
+        // POST: Class1/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "NumberID,Name,LastName,age")] Class1 class1)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(class1).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(class1);
+        }
+
+        // GET: Class1/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Class1 class1 = db.Classes.Find(id);
+            if (class1 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(class1);
+        }
+
+        // POST: Class1/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Class1 class1 = db.Classes.Find(id);
+            db.Classes.Remove(class1);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Random APP/Models/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace Random_APP.Models
+{
+    public class Class1
+
+    {
+
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        [Key]
+        public int NumberID { get; set; }
+        public int age { get; set; }
+
+
+    }
+
+    public class ClassDBContext : DbContext
+    {
+        public ClassDBContext() : base("OnlineDB") { }
+        public DbSet<Class1> Classes { get; set; }
+    }
+
+}
